bbeadec8e9c19536be53b6fc5e31c9bd86c9e1e2
Bob <bobvanhooff@outlook.com>
2019-06-07 00:34:01 +0200

    Added -metadata option

--- Slipe-CLI/Commands/Project/Compilation/CompileCommand.cs
+++ Slipe-CLI/Commands/Project/Compilation/CompileCommand.cs
@@ -106,7 +106,7 @@ namespace Slipe.Commands.Project
 
         private void CompileSourceFiles(string directory, string to, string[] dlls, string[] attributes, bool isModule = false)
         {
-            string command = @"dotnet ./Slipe/Compiler/CSharp.lua.Launcher.dll -s " + directory + @" -d " + to + " -c";
+            string command = @"dotnet ./Slipe/Compiler/CSharp.lua.Launcher.dll -s -metadata " + directory + @" -d " + to + " -c";
             if(attributes.Length > 0)
             {
                 command += " -a " + string.Join(";", attributes);
