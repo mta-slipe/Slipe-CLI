1994321ae5df241fba18c686cf7225f36b6b7df8
Bob <bobvanhooff@outlook.com>
2020-01-26 13:02:56 +0100

    Azure storage URLs

--- Urls/Urls.cs
+++ Urls/Urls.cs
@@ -6,17 +6,17 @@ namespace SlipeUrls
     {
         public const string analyticsUrl = "https://slipeanalytics.azurewebsites.net/api/StoreAnalytics";
 
-        public const string windowsCliInstallerUrl = "https://mta-slipe.com/downloads/SlipeInstaller.exe";
-        public const string devWindowsCliInstallerUrl = "https://development.mta-slipe.com/downloads/SlipeInstaller.exe";
+        public const string windowsCliInstallerUrl = "https://slipestorage.blob.core.windows.net/cli-production/SlipeInstaller.exe";
+        public const string devWindowsCliInstallerUrl = "https://slipestorage.blob.core.windows.net/cli-development/SlipeInstaller.exe";
 
-        public const string windowsCliUrl = "https://mta-slipe.com/downloads/cli.zip";
-        public const string devWindowsCliUrl = "https://development.mta-slipe.com/downloads/cli.zip";
+        public const string windowsCliUrl = "https://slipestorage.blob.core.windows.net/cli-production/cli.zip";
+        public const string devWindowsCliUrl = "https://slipestorage.blob.core.windows.net/cli-development/cli.zip";
 
-        public const string linuxCliUrl = "https://mta-slipe.com/downloads/cli-linux.zip";
-        public const string devLinuxCliUrl = "https://development.mta-slipe.com/downloads/cli-linux.zip";
+        public const string linuxCliUrl = "https://slipestorage.blob.core.windows.net/cli-production/cli-linux.zip";
+        public const string devLinuxCliUrl = "https://slipestorage.blob.core.windows.net/cli-development/cli-linux.zip";
 
-        public const string resourceTemplateUrl = "https://mta-slipe.com/downloads/core.zip";
-        public const string devResourceTemplateUrl = "https://development.mta-slipe.com/downloads/core.zip";
+        public const string resourceTemplateUrl = "https://slipestorage.blob.core.windows.net/resource-production/resource.zip";
+        public const string devResourceTemplateUrl = "https://slipestorage.blob.core.windows.net/resource-development/resource.zip";
 
         public const string cliDocs = "https://mta-slipe.com/docs/cli.html";
     }
